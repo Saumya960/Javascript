ba7770a5e904fe0ad2b4127a47866ebfbc5eb781
Saumya960 <saumyap.960602@gmail.com>
2024-11-08 19:59:40 +0200

    hghg

--- Module1/Task4/Task4.js
+++ Module1/Task4/Task4.js
@@ -1,21 +1,18 @@
 'use strict';
 
 window.onload = function sortStudent() {
-    // Prompt the user to enter their name
+
     const name = prompt("Please enter your name:");
 
-    // Check if name is not empty or null
     if (!name) {
         document.getElementById("result").innerText = "You didn't enter a name!";
         return;
     }
 
-    // Generate a random number between 1 and 4
     const randomNum = Math.floor(Math.random() * 4) + 1;
 
-    let house; // Variable to store the house name
+    let house;
 
-    // Assign the house based on the random number
     switch (randomNum) {
         case 1:
             house = "Gryffindor";
@@ -33,6 +30,5 @@ window.onload = function sortStudent() {
             house = "Unknown House"; // This should never happen
     }
 
-    // Display the result
     document.getElementById("result").innerText = `${name}, you are ${house}!`;
 }
